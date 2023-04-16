a5ed136d2c531b703895deb4965d3f12ab01d622
PatricioAguilera <patricioaguilera4524@gmail.com>
2023-04-16 20:18:26 -0300

    Se completo el Ejercicio

---
+++ Practico/TP1/TP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    /* Solicitar al usuario que ingrese la base y altura de un rectángulo, y calcular y mostrar
+por pantalla el área y perímetro del mismo */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bas, alt, area, peri;
+
+            Console.WriteLine("Ingrese la base del rectangulo: ");
+            bas=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del rectangulo: ");
+            alt=int.Parse(Console.ReadLine());
+
+            area= bas*alt;
+            peri=2*alt+2*area;
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("La area del Rectangulo es: " + area);
+            Console.WriteLine("El perimetro del Rectangulo es: "+peri);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Practico/TP2/TP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    /* Escribir un programa que calcule el volumen de un cilindro. Para ello se deberá
+solicitar al usuario que ingrese el radio y la altura. Mostrar el resultado por pantalla. */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double alt, vol, radio;
+
+            Console.WriteLine("Ingrese el radio del Cilindro: ");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del Cilindro: ");
+            alt = int.Parse(Console.ReadLine());
+
+            vol = (Math.PI * (radio * radio) * alt);
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("El volumen del Cilindro es de: "+vol);
+            Console.WriteLine();
+
+        }
+    }
+}
