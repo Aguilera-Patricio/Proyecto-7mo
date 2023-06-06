9df23f5da838cf03916615f7893b2f84206a69b4
Patricio Aguilera <patricioaguilera4524@gmail.com>
2023-06-05 22:32:02 -0300

    Se completo el Ejercicio

--- Practico-10/TP5/TP5/Form1.Designer.cs
+++ Practico-10/TP5/TP5/Form1.Designer.cs
@@ -28,14 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             btnIniciar = new Button();
             etLabel = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            btnDetener = new Button();
+            btnReiniciar = new Button();
+            label2 = new Label();
             SuspendLayout();
             // 
             // btnIniciar
             // 
             btnIniciar.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            btnIniciar.Location = new Point(318, 335);
+            btnIniciar.Location = new Point(152, 368);
             btnIniciar.Name = "btnIniciar";
             btnIniciar.Size = new Size(125, 51);
             btnIniciar.TabIndex = 2;
@@ -46,12 +51,50 @@
             // etLabel
             // 
             etLabel.AutoSize = true;
-            etLabel.Font = new Font("Algerian", 72F, FontStyle.Bold, GraphicsUnit.Point);
-            etLabel.Location = new Point(139, 98);
+            etLabel.Font = new Font("Algerian", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            etLabel.Location = new Point(134, 197);
             etLabel.Name = "etLabel";
-            etLabel.Size = new Size(496, 160);
+            etLabel.Size = new Size(541, 106);
             etLabel.TabIndex = 3;
-            etLabel.Text = "Reloj";
+            etLabel.Text = "00 : 00 : 00";
+            etLabel.Click += etLabel_Click_1;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // btnDetener
+            // 
+            btnDetener.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDetener.Location = new Point(340, 368);
+            btnDetener.Name = "btnDetener";
+            btnDetener.Size = new Size(125, 51);
+            btnDetener.TabIndex = 4;
+            btnDetener.Text = "Detener";
+            btnDetener.UseVisualStyleBackColor = true;
+            btnDetener.Click += btnDetener_Click;
+            // 
+            // btnReiniciar
+            // 
+            btnReiniciar.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnReiniciar.Location = new Point(523, 368);
+            btnReiniciar.Name = "btnReiniciar";
+            btnReiniciar.Size = new Size(125, 51);
+            btnReiniciar.TabIndex = 5;
+            btnReiniciar.Text = "Reiniciar";
+            btnReiniciar.UseVisualStyleBackColor = true;
+            btnReiniciar.Click += btnReiniciar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Algerian", 72F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(152, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(496, 160);
+            label2.TabIndex = 6;
+            label2.Text = "Reloj";
             // 
             // Form1
             // 
@@ -59,6 +102,9 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(253, 249, 196);
             ClientSize = new Size(811, 454);
+            Controls.Add(label2);
+            Controls.Add(btnReiniciar);
+            Controls.Add(btnDetener);
             Controls.Add(etLabel);
             Controls.Add(btnIniciar);
             Name = "Form1";
@@ -74,5 +120,9 @@
         private Label label1;
         private Button btnIniciar;
         private Label etLabel;
+        private System.Windows.Forms.Timer timer1;
+        private Button btnDetener;
+        private Button btnReiniciar;
+        private Label label2;
     }
 }
--- Practico-10/TP5/TP5/Form1.cs
+++ Practico-10/TP5/TP5/Form1.cs
@@ -2,6 +2,8 @@ namespace TP5
 {
     public partial class Form1 : Form
     {
+        private int h = 0, m = 0, s = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,37 +11,69 @@ namespace TP5
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
-            int h = 0, m = 0, s = 0;
-
-            do
-            {
-                etLabel.Text = h + " : " + m + " : " + s;
-
-                s = s + 1;
-
-                MessageBox.Show(h + " : " + m + " : " + s);
-
-                if (s == 59)
-                {
-                    m = m + 1;
-
-                    s = 0;
-
-                    if (m == 59)
-                    {
-                        h = h + 1;
-
-                        m = 0;
-                    }
-                }
-
-            } while (h < 24);
-
+            timer1.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            etLabel.Text = h + " : " + m + " : " + s;
+
+            s = s + 1;
+
+            if (s == 60)
+            {
+                m = m + 1;
+
+                s = 0;
+
+                if (m == 60)
+                {
+                    h = h + 1;
+
+                    m = 0;
+
+                    if (h == 24)
+                    {
+                        h = 0;
+                        m = 0;
+                        s = 0;
+
+                        etLabel.Text = h + " : " + m + " : " + s;
+                    }
+                }
+            }
+
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void etLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            h = 0;
+            m = 0;
+            s = 0;
+
+            etLabel.Text = h + " : " + m + " : " + s;
+        }
+
+        private void etLabel_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
